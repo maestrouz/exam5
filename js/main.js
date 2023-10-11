d20f3a61a5c4855044268eb9945604905f27e9b5
WebCodersDeveloper <ibrohimahmedov869@gmail.com>
2023-10-11 18:48:53 +0500

    Edit in js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-// import {GetArray} from "../js/index";
-// console.log(GetArray());
-
 import { object } from "./project.js";
-
+const cardList = document.getElementById('card_list')
 let api = object;
-console.log(api);
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    let render = api.map((item) => {
+        return `
+        <div class="card" data-id="${item.id}">
+            <img src="${item.img}" alt="img" />
+            <div class="scene">
+            <p>${item.pirce}</p>
+            </div>
+            <h3>${item.decription}</h3>
+            <button class="korzina">Savatga</button>
+        </div>
+        `
+    })
+    render = render.join(' ');
+    cardList.innerHTML = render;
+})
