f6856d7b1a3e3865e7297c0f3005e06c94725807
WebCodersDeveloper <ibrohimahmedov869@gmail.com>
2023-10-11 15:54:47 +0500

    edit

---
+++ js/object.js
@@ -0,0 +1,18 @@
+let object = [
+    {   id: 5694,
+        name: "Kalbasa",
+        img: "./img/image (3).png",
+        decription: "Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»",
+        pirce :"599,99₽"
+    },
+    {
+        img: "./img/image (1).png",
+        decription: "Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...",
+        pirce :"44,50₽"
+    },
+    {
+        img: "./img/image (2).png",
+        decription: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+        pirce :"44,50₽"
+    },
+]
